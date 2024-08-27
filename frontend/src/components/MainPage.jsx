d58fc87e4bebee07afbe89a14ec47992c2c2dfdc
warriorBunny013 <mona23sonai@gmail.com>
2024-08-27 17:36:43 +0530

    removed popup border error

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -200,7 +200,7 @@ const MainPage = () => {
 
       {/* Mobile-only popup */}
       {isPopupVisible && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black border border-white bg-opacity-80">
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-80">
           <div className="relative bg-black border border-white  text-white p-6 rounded-lg shadow-lg max-w-sm w-full mx-auto">
             <button className="absolute top-2 right-2 text-white text-2xl font-bold" onClick={closePopup}>
               &times;
