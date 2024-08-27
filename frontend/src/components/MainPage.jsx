17c451897f3a98fa273f406efbb0650f87186734
warriorBunny013 <mona23sonai@gmail.com>
2024-08-27 14:05:36 +0530

    added desktop mail mobility

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -70,7 +70,17 @@ const MainPage = () => {
             </ul>
           </div>
           <div className="navbar-end">
-            <a href="mailto:Solsticetarot143@gmail.com"  className="btn border-2 border-white">Contact me</a>
+            <a href="mailto:Solsticetarot143@gmail.com" onClick={(e) => {
+      e.preventDefault(); // Prevent default mailto behavior
+      const isDesktop = window.matchMedia("(min-width: 768px)").matches;
+      const mailtoLink = "mailto:Solsticetarot143@gmail.com";
+
+      if (isDesktop) {
+        window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=Solsticetarot143@gmail.com`, '_blank');
+      } else {
+        window.location.href = mailtoLink; // Fallback for mobile devices
+      }
+    }}  className="btn border-2 border-white">Contact me</a>
           </div>
         </motion.div>
 
@@ -154,7 +164,17 @@ const MainPage = () => {
             <span>
               <ul className="flex flex-wrap gap-y-4 items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li><a href="#about" onClick={() => navigateTo('about')} className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">About</a></li>
-                <li><a href="mailto:Solsticetarot143@gmail.com" className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Contact</a></li>
+                <li><a href="mailto:Solsticetarot143@gmail.com" onClick={(e) => {
+      e.preventDefault(); // Prevent default mailto behavior
+      const isDesktop = window.matchMedia("(min-width: 768px)").matches;
+      const mailtoLink = "mailto:Solsticetarot143@gmail.com";
+
+      if (isDesktop) {
+        window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=Solsticetarot143@gmail.com`, '_blank');
+      } else {
+        window.location.href = mailtoLink; // Fallback for mobile devices
+      }
+    }} className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Contact</a></li>
                 <li><a href="#services" onClick={() => navigateTo('services')} className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Services</a></li>
                 <li><a href="#testimonials" onClick={() => navigateTo('testimonials')} className="hover:underline me-4 cursor-pointer md:me-6 block py-2 px-4 text-white">Testimonials</a></li>
               </ul>
