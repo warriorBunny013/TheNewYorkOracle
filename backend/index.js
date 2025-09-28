31506f3f7f6bb9d4bb1341c106cd3f6fa02f4469
warriorBunny013 <mona23sonai@gmail.com>
2025-09-28 14:16:26 +0530

    added development mode frontend url

--- backend/index.js
+++ backend/index.js
@@ -83,8 +83,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin: process.env.NODE_ENV === "production" 
-      ? ["https://www.soulsticetarot.com", "https://soulsticetarot.com","the-new-york-oracle-development-mod.vercel.app"]
-      : ["http://localhost:3000", "http://localhost:8080","the-new-york-oracle-development-mod.vercel.app"],
+      ? ["https://www.soulsticetarot.com", "https://soulsticetarot.com","the-new-york-oracle-development-mod.vercel.app,https://the-new-york-oracle-develo-git-224dce-warriorbunny013s-projects.vercel.app"]
+      : ["http://localhost:3000", "http://localhost:8080","the-new-york-oracle-development-mod.vercel.app","https://the-new-york-oracle-develo-git-224dce-warriorbunny013s-projects.vercel.app"],
     methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
